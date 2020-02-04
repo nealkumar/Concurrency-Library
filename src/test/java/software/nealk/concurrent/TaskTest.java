f67ee2bc905ada20589046860ea35f2313ec983b
Neal Kumar <nkumar20@jcu.edu>
2020-02-03 21:20:41 -0500

    Added basic unit tests for NonRetrievableTask.java, as well as as updated the group/artifact id to more closely resemble standard java packaging nomenclature.

---
+++ src/test/java/software/nealk/concurrent/TaskTest.java
@@ -0,0 +1,14 @@
+package software.nealk.concurrent;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TaskTest {
+	
+	@Test
+	public void iTaskTest(){
+		assertTrue(true);
+	}
+
+}
