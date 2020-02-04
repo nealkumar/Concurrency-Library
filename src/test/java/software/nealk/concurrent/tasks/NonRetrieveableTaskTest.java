d7ac15790d57f7ead99a73790d4e88d6fd6c4e98
Neal Kumar <nkumar20@jcu.edu>
2020-02-03 21:26:29 -0500

    updated NonRetrieveableTaskTest.java unit test

---
+++ src/test/java/software/nealk/concurrent/tasks/NonRetrieveableTaskTest.java
@@ -0,0 +1,11 @@
+package software.nealk.concurrent.tasks;
+
+import static org.junit.Assert.assertTrue;
+
+public class NonRetrieveableTaskTest {
+	
+	public void test() {
+		assertTrue(true);
+	}
+
+}
