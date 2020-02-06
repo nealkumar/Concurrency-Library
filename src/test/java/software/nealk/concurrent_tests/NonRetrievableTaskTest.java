342050a0be83e49beec29878f130a958e31f7c70
Neal Kumar <nealk@nealk.com>
2020-02-05 21:42:19 -0500

    Unit testing (#14)
    * Added unit test for NonRetrievableTask.getVal() to throw an UnsupportedOperationException in src/java/test/software/nealk/concurrent_tests/NonRetrievableTaskTest.java
    
    * Update README.md
    
    * Added unit tests for run() and execute() in sofware.nealk.concurrent.tasks.NonRetrievableTaskTest.java
    
    * Update README.md
    
    * Create version_release_proposal.md
    
    * updated maven surefire plugin
    
    * reverted pom.xml versioning back to 0.8 (0.9 with more coverage, 1.0 to be released with even more coverage)
    
    * Pre-release (v0.2.7) update of versioning in pom.xml
    
    * [maven-release-plugin] prepare release v0.2.7
    
    * [maven-release-plugin] prepare for next development iteration
    
    * [maven-release-plugin] prepare release v0.4.1
    
    * [maven-release-plugin] prepare for next development iteration

--- src/test/java/software/nealk/concurrent_tests/NonRetrievableTaskTest.java
+++ src/test/java/software/nealk/concurrent_tests/NonRetrievableTaskTest.java
@@ -15,6 +15,24 @@ public class NonRetrievableTaskTest {
 				+ "See: src/test/java/software/nealk/concurrent_tests/NonRetrievableTaskTest.java", t);
 	}
 	
+	@Test(expected = UnsupportedOperationException.class)
+	public void checkForUnsupportedOperationExceptionTask() throws InterruptedException {
+		NonRetrievableTask r = new NR();
+		r.getVal();
+	}
+	
+	@Test
+	public void checkExecuteMethod() {
+		NR r = new NR();
+		r.execute();
+	}
+	
+	@Test
+	public void checkRunMethod() {
+		Runnable r = new NR();
+		r.run();
+	}
+	
 	
 	private class NR extends NonRetrievableTask{
 		@Override
