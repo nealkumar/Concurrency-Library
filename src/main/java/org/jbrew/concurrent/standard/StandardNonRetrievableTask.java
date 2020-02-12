5ee993976da98475762c6964c434fcbc4210dcaf
Neal Kumar <nkumar20@jcu.edu>
2020-02-12 15:06:43 -0500

    added example standard nrt under new package - standard.

---
+++ src/main/java/org/jbrew/concurrent/standard/StandardNonRetrievableTask.java
@@ -0,0 +1,18 @@
+package org.jbrew.concurrent.standard;
+
+import org.jbrew.concurrent.NonRetrievableTask;
+
+/**
+ * This is a standard implementation of the NonRetrievableTask.
+ * @author Neal Kumar
+ *
+ */
+public class StandardNonRetrievableTask extends NonRetrievableTask{
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
