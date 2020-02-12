b464507be505558fb1ba902a42c3350ab09880f4
Neal Kumar <nkumar20@jcu.edu>
2020-02-12 17:10:15 -0500

    updated RetrievableTask.java for javadocs

--- src/main/java/org/jbrew/concurrent/RetrievableTask.java
+++ src/main/java/org/jbrew/concurrent/RetrievableTask.java
@@ -24,17 +24,18 @@ public abstract class RetrievableTask<T> implements Task<T>{
 	protected abstract void execute();
 	
 	/**
-	 * Sets the value of <code>obj</code> in a ThreadSafe manner.
+	 * Sets the value of the <code>obj</code> in a {@link ThreadSafe} manner.
 	 * @param obj The object to set.
 	 */
 	@ThreadSafe
 	protected abstract void setVal(T obj);
 
-	/**
-	 * Blocks until a condition is met (such as the excute() method completing or <code>obj</code> of type &lt;T&gt; is not null {@link org.jbrew.concurrent.MethodBlockingRetrievableTask} and @see org.jbrew.concurrent.retrievable.ObjectBlockingRetrievableTask. Returns the <code>obj</code>.
+	/**<p>
+	 * Gets and returns the value of the <code>obj</code> in a {@link ThreadSafe} manner. The method blocks until a condition is met (such as the excute() method returning or the <code>obj</code> of type &lt;T&gt; being set (via the setVal() method). For more information on the way internal contention is handled, please refer to {@link org.jbrew.concurrent.MethodBlockingRetrievableTask} and {@link org.jbrew.concurrent.ObjectBlockingRetrievableTask}.
 	 * <br><br>
 	 * Note that compile-time type checking is enabled. The usage 
 	 * of Java Generics nullifies the compiler warning "unchecked".
+	 * </p>
 	 */
 	@SuppressWarnings("unchecked") 
 	@Override 
