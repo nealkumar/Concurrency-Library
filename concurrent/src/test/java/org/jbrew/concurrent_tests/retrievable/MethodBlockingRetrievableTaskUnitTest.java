d4fc0b04691424c829aa1df649413ed3fceedb74
Neal Kumar <nealk@nealk.com>
2020-02-16 20:49:06 -0500

    added mbrt unit test for run()

---
+++ concurrent/src/test/java/org/jbrew/concurrent_tests/retrievable/MethodBlockingRetrievableTaskUnitTest.java
@@ -0,0 +1,38 @@
+package org.jbrew.concurrent_tests.retrievable;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jbrew.concurrent.MethodBlockingRetrievableTask;
+import org.jbrew.concurrent.RetrievableTask;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MethodBlockingRetrievableTaskUnitTest {
+	
+	private RetrievableTask<String> retrievableTask;
+	private Thread t;
+	private String setMessage;
+	
+	@Before
+	private void setup() {
+		this.retrievableTask = new MBRT<String>();
+		this.t = new Thread(retrievableTask);
+		this.setMessage = "MBRT Val has been set.";
+	}
+	
+	@Test
+	private void retrievableTaskRunTest() throws InterruptedException {
+		t.run();
+		assertEquals(this.setMessage, this.retrievableTask.getVal());
+	}
+	
+	
+	private class MBRT<T> extends MethodBlockingRetrievableTask<String>{
+		@Override
+		protected void execute() {
+			// TODO Auto-generated method stub
+			this.setVal(setMessage);
+		}
+	}
+
+}
