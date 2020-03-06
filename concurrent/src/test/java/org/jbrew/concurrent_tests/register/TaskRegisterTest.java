0b6d01da7e75f54b2cf132f79836859ca88253bf
Neal Kumar <nkumar20@jcu.edu>
2020-03-05 22:26:11 -0500

    Created unit tests for TaskRegister.java in tests/register/TaskRegisterTest.java.

---
+++ concurrent/src/test/java/org/jbrew/concurrent_tests/register/TaskRegisterTest.java
@@ -0,0 +1,37 @@
+package org.jbrew.concurrent_tests.register;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.jbrew.concurrent.*;
+
+@org.jbrew.Testing
+public class TaskRegisterTest {
+	
+	private Task<Integer> task;
+	
+	@org.junit.Before
+	public void setup() {
+		task = new BT<>();
+	}
+	
+	@Test
+	public void instantiateTaskRegisterTest() {
+		assertNotNull(new TaskRegister());
+	}
+	
+	@Test
+	public void offerTaskTest() {
+		TaskRegister register = new TaskRegister();
+		register.offerTask(task);
+		assert register.pollTask() == this.task;
+	}
+	
+	private class BT<T> extends ObjectBlockingTask<Integer>{
+		@Override
+		protected void execute() {
+			this.accept(69);
+		}
+	}
+
+}
