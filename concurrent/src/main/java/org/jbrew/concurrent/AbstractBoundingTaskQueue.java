bb7b55cb2ef91052fa7ae841b997b2801460cf55
Neal Kumar <nkumar20@jcu.edu>
2020-03-09 00:26:24 -0400

    Implemented AbstractBlockingTaskQueue#dequeue() template method using synchronized blocks and wait/notifyAll() patterns.

---
+++ concurrent/src/main/java/org/jbrew/concurrent/AbstractBoundingTaskQueue.java
@@ -0,0 +1,33 @@
+package org.jbrew.concurrent;
+
+/**
+ * A {@link AbstractBoundingTaskQueue} provides a skeletal implementation of the {@link org.jbrew.concurrent.TaskQueue} interface.
+ * To implement an un/bounded queue of {@link org.jbrew.concurrent.Task}, the programmer only needs to extend this class
+ * and concretely implement its abstract methods. 
+ * @author nealk
+ *
+ */
+public abstract class AbstractBoundingTaskQueue implements TaskQueue<Task<?>>{
+	
+	protected java.util.Queue<Task<? extends Object>> queue;
+	
+	@Override
+	public final Task<? extends Object> dequeue(){
+		Task<?> task = null;
+		try {
+			synchronized(this) {
+				while(this.queue.size() <= 0) this.wait();
+				task = queue.poll();
+				this.notifyAll();
+			}
+		} catch(NullPointerException npe){
+			System.out.println("TaskQueue class has NOT been initialized! You first need to instantiate your "
+					+ "TaskQueue object before attempting your operation.");
+			npe.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return task;
+	}
+
+}
