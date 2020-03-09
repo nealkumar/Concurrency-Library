fba761392e3595fac87277dce5b6ce36d74680f9
Neal Kumar <nkumar20@jcu.edu>
2020-03-09 00:00:40 -0400

    Created class headers for BoundedTaskQueue.java and UnboundedTaskQueue.java.

--- concurrent/src/main/java/org/jbrew/concurrent/UnboundedTaskQueue.java
+++ concurrent/src/main/java/org/jbrew/concurrent/UnboundedTaskQueue.java
@@ -1,5 +1,12 @@
 package org.jbrew.concurrent;
 
+/**
+ * A {@link BoundedTaskQueue} is an unordered, unbounded implementation of a {@link org.jbrew.concurrent.TaskQueue}. 
+ * This implementation is {@link org.jbrew.concurrent.ThreadSafe}, as it can safely handle multiple consumer
+ * and producer threads writing and/or reading from it.
+ * @author nealk
+ */
+@ThreadSafe
 public class UnboundedTaskQueue {
 
 }
