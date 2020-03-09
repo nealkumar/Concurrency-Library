c12dc5e407ba0f3db6946b510536ff2a56a8fef8
Neal Kumar <nkumar20@jcu.edu>
2020-03-08 23:54:56 -0400

    Updated Javadocs for BlockingTaskQueue.java

--- concurrent/src/main/java/org/jbrew/concurrent/BlockingTaskQueue.java
+++ concurrent/src/main/java/org/jbrew/concurrent/BlockingTaskQueue.java
@@ -1,7 +1,7 @@
 package org.jbrew.concurrent;
 
 /**
- * A {@link BlockingTaskQueue} is an unordered queue of one or more {@link org.jbrew.concurrent.Task}s.
+ * A {@link BlockingTaskQueue} is an unordered bounded implementation of a {@link org.jbrew.concurrent.TaskQueue}. 
  * This implementation is {@link org.jbrew.concurrent.ThreadSafe}, as it can safely handle multiple consumer
  * and producer threads writing and/or reading from it.
  * @author nealk
