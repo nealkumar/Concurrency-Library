bdb2893a45a20664c3f6baeeb2b0c140f92bed7f
Neal Kumar <nealk@nealk.com>
2020-03-10 12:25:45 -0400

    Updated nomenclature in AbstractBoundingTaskQueue.java to AbstractTaskQueue.java to more effectively describe its functionality.

--- concurrent/src/main/java/org/jbrew/concurrent/AbstractTaskQueue.java
+++ concurrent/src/main/java/org/jbrew/concurrent/AbstractTaskQueue.java
@@ -1,13 +1,13 @@
 package org.jbrew.concurrent;
 
 /**
- * A {@link AbstractBoundingTaskQueue} provides a skeletal implementation of the {@link org.jbrew.concurrent.TaskQueue} interface.
+ * A {@link AbstractTaskQueue} provides a skeletal implementation of the {@link org.jbrew.concurrent.TaskQueue} interface.
  * To implement an un/bounded queue of {@link org.jbrew.concurrent.Task}, the programmer only needs to extend this class
  * and concretely implement its abstract methods. 
  * @author nealk
  *
  */
-public abstract class AbstractBoundingTaskQueue implements TaskQueue<Task<?>>{
+public abstract class AbstractTaskQueue implements TaskQueue<Task<?>>{
 	
 	protected java.util.Queue<Task<? extends Object>> queue;
 	
