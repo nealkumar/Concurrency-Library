fe69dbf47fef8883097a6beca393a795321b42e5
Neal Kumar <nkumar20@jcu.edu>
2020-03-13 02:20:13 -0400

    Added unit tests for {@link org.jbrew.concurrent.AbstractBlockingTaskQueue#dequeueDev} with {@link java.lang.InterruptedException} using the Java Reflections API.

--- concurrent/src/test/java/org/jbrew/concurrent_tests/blocking_task_queue/AbstractBlockingTaskQueueTest.java
+++ concurrent/src/test/java/org/jbrew/concurrent_tests/blocking_task_queue/AbstractBlockingTaskQueueTest.java
@@ -1,5 +1,6 @@
 package org.jbrew.concurrent_tests.blocking_task_queue;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -8,11 +9,13 @@ import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Optional;
 import java.util.Queue;
 
 import org.jbrew.concurrent.AbstractBlockingTaskQueue;
 import org.jbrew.concurrent.BasicTask;
 import org.jbrew.concurrent.BoundedTaskQueue;
+import org.jbrew.concurrent.MethodBlockingTask;
 import org.jbrew.concurrent.Task;
 import org.jbrew.concurrent.UnboundedTaskQueue;
 import org.junit.After;
@@ -20,11 +23,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class AbstractBlockingTaskQueueTest {
-	
+
 	private Task<? extends Object> shouldBeNull;
+	private Method method;
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 	private final PrintStream originalErr = System.err;
-	
+
 	@Before
 	public void setUpStreams() {
 		System.setErr(new PrintStream(errContent));
@@ -34,68 +38,73 @@ public class AbstractBlockingTaskQueueTest {
 	public void restoreStreams() {
 		System.setErr(originalErr);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Test
-	public void queueNullBoundedTest() throws SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
+	public void queueNullBoundedTest()
+			throws SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
 		AbstractBlockingTaskQueue taskQueue = new BoundedTaskQueue(3);
 		Field field = taskQueue.getClass().getSuperclass().getDeclaredField("queue");
 		field.setAccessible(true);
 		Queue<Task<?>> queue = (Queue<Task<?>>) field.get(taskQueue);
 		assertNotNull(queue);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Test
-	public void queueNullUnboundedTest() throws SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
+	public void queueNullUnboundedTest()
+			throws SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
 		AbstractBlockingTaskQueue taskQueue = new UnboundedTaskQueue();
 		Field field = taskQueue.getClass().getSuperclass().getDeclaredField("queue");
 		field.setAccessible(true);
 		Queue<Task<?>> queue = (Queue<Task<?>>) field.get(taskQueue);
 		assertNotNull(queue);
 	}
-	
+
 	/**
-	 * Tests the ability for the {@link org.jbrew.concurrent.AbstractBlockingTaskQueue#dequeue()} to block while 
-	 * its internal queue size is empty.
-	 * @throws InterruptedException 
+	 * Tests the ability for the
+	 * {@link org.jbrew.concurrent.AbstractBlockingTaskQueue#dequeue()} to block
+	 * while its internal queue size is empty.
+	 * 
+	 * @throws InterruptedException
 	 */
 	@Test
 	public void dequeueBlockingAtSize0Test() throws InterruptedException {
 		UnboundedTaskQueue dummy = new UnboundedTaskQueue();
-		Thread t = new Thread(() ->{
+		Thread t = new Thread(() -> {
 			shouldBeNull = dummy.dequeue();
 		});
 		t.start();
 		long start = System.nanoTime();
-		t.join(1000);	//force-terminate the thread after 1 second and return back its state as-is
+		t.join(1000); // force-terminate the thread after 1 second and return back its state as-is
 		long end = System.nanoTime();
-		long delta = start-end;
-		double delta_d = (double) delta / 1_000_000_000.0;	//convert from nanoseconds to milliseconds
+		long delta = start - end;
+		double delta_d = (double) delta / 1_000_000_000.0; // convert from nanoseconds to milliseconds
 		assert 1.0 >= delta_d;
 	}
-	
+
 	@Test
 	public void dequeueBlockingAtSize0Test2() throws InterruptedException {
 		UnboundedTaskQueue dummy = new UnboundedTaskQueue();
-		Thread t = new Thread(() ->{
+		Thread t = new Thread(() -> {
 			shouldBeNull = dummy.dequeue();
 		});
 		t.start();
 		t.join(50);
 		assertNull(shouldBeNull);
 	}
-	
+
 	@Test
-	public void dequeueBasicTest() throws InterruptedException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+	public void dequeueBasicTest() throws InterruptedException, IllegalArgumentException, IllegalAccessException,
+			NoSuchFieldException, SecurityException {
 		AbstractBlockingTaskQueue dummy = new UnboundedTaskQueue();
 		SpinTask task = new SpinTask();
-		//enqueue two skeleton tasks
+		// enqueue two skeleton tasks
 		dummy.enqueue(task);
 		dummy.enqueue(task);
-		//dequeue one task... there should now only be one task in the internal queue.
+		// dequeue one task... there should now only be one task in the internal queue.
 		dummy.dequeue();
-		//check internal queue size via reflection to ensure its size == 1
+		// check internal queue size via reflection to ensure its size == 1
 		Class<?> clazz = AbstractBlockingTaskQueue.class;
 		Field field = clazz.getDeclaredField("queue");
 		field.setAccessible(true);
@@ -103,11 +112,52 @@ public class AbstractBlockingTaskQueueTest {
 		Queue<Task<?>> queue = (Queue<Task<?>>) field.get(dummy);
 		assert queue.size() == 1;
 	}
-	
-	@Test//(expected = InterruptedException.class)
-	public void dequeueBasicInterruptedExceptionTest() throws InterruptedException{
+
+	@Test
+	public void dequeueDevBasicTest() throws InterruptedException, IllegalArgumentException, IllegalAccessException,
+			NoSuchFieldException, SecurityException, NoSuchMethodException, InvocationTargetException {
+		AbstractBlockingTaskQueue dummy = new UnboundedTaskQueue();
+		SpinTask task = new SpinTask();
+		SpinTaskOptional<Optional<?>> opTask = new SpinTaskOptional<>();
+		Class<?> clazz = AbstractBlockingTaskQueue.class;
+		Field field = clazz.getDeclaredField("devQueue");
+		field.setAccessible(true);
+		@SuppressWarnings("unchecked")
+		Queue<Task<Optional<?>>> queue = (Queue<Task<Optional<?>>>) field.get(dummy);
+		// enqueue two skeleton tasks
+		queue.offer(opTask);
+		queue.offer(opTask);
+		queue.offer(opTask);
+		// dequeue one task... there should now only be one task in the internal queue.
+		Thread t = new Thread(() -> {
+			try {
+				method = clazz.getDeclaredMethod("dequeueDev");
+			} catch (NoSuchMethodException | SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			method.setAccessible(true);
+			try {
+				method.invoke(dummy);
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		t.start();
+		t.join(10);
+		// check internal queue size via reflection to ensure its size == 1
+		//Field devQueue = clazz.getDeclaredField("devQueue");
+		//field.setAccessible(true);
+		//@SuppressWarnings("unchecked")
+		//Queue<Task<?>> queue = (Queue<Task<?>>) field.get(dummy);
+		assertEquals(2, queue.size());
+	}
+
+	@Test // (expected = InterruptedException.class)
+	public void dequeueBasicInterruptedExceptionTest() throws InterruptedException {
 		UnboundedTaskQueue dummy = new UnboundedTaskQueue();
-		Thread t = new Thread(() ->{
+		Thread t = new Thread(() -> {
 			shouldBeNull = dummy.dequeue();
 		});
 		t.start();
@@ -115,10 +165,11 @@ public class AbstractBlockingTaskQueueTest {
 		t.join();
 		assert errContent.toString().contains("InterruptedException");
 	}
-	
+
 	@Test
-	public void dequeueDevBasicInterruptedExceptionTest() throws InterruptedException, NoSuchMethodException, SecurityException{
-		//Invoke method via reflection.
+	public void dequeueDevBasicInterruptedExceptionTest()
+			throws InterruptedException, NoSuchMethodException, SecurityException {
+		// Invoke method via reflection.
 		AbstractBlockingTaskQueue taskQueue = new BoundedTaskQueue(3);
 		Thread t = new Thread(() -> {
 			Class<?> clazz = AbstractBlockingTaskQueue.class;
@@ -139,38 +190,49 @@ public class AbstractBlockingTaskQueueTest {
 		t.interrupt();
 		t.join();
 		assert errContent.toString().contains("InterruptedException");
-	}
-	
-	@Test
-	public void dequeueDevBasicBasicTest() throws InterruptedException, NoSuchMethodException, SecurityException{
-		//Invoke method via reflection.
-		AbstractBlockingTaskQueue taskQueue = new BoundedTaskQueue(3);
-		Thread t = new Thread(() -> {
-			Class<?> clazz = AbstractBlockingTaskQueue.class;
-			Method method = null;
-			try {
-				method = clazz.getDeclaredMethod("dequeueDev");
-			} catch (NoSuchMethodException | SecurityException e1) {
-				e1.printStackTrace();
-			}
-			method.setAccessible(true);
-			try {
-				method.invoke(taskQueue);
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
-		});
-		t.start();
-		t.interrupt();
-		t.join();
-		assert errContent.toString().contains("InterruptedException");
-	}
-	
-	private class SpinTask extends BasicTask{
-		@Override
-		protected void execute() {
-			for(int i=0;i<100;i++) {/** intentionally empty */}
-		}
 	}
 
+	@Test
+	public void dequeueDevBasicBasicInterruptedExceptionTest()
+			throws InterruptedException, NoSuchMethodException, SecurityException {
+		// Invoke method via reflection.
+		AbstractBlockingTaskQueue taskQueue = new BoundedTaskQueue(3);
+		Thread t = new Thread(() -> {
+			Class<?> clazz = AbstractBlockingTaskQueue.class;
+			Method method = null;
+			try {
+				method = clazz.getDeclaredMethod("dequeueDev");
+			} catch (NoSuchMethodException | SecurityException e1) {
+				e1.printStackTrace();
+			}
+			method.setAccessible(true);
+			try {
+				method.invoke(taskQueue);
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		});
+		t.start();
+		t.interrupt();
+		t.join();
+		assert errContent.toString().contains("InterruptedException");
+	}
+
+	private class SpinTask extends BasicTask {
+		@Override
+		protected void execute() {
+			for (int i = 0; i < 100; i++) {
+				/** intentionally empty */
+			}
+		}
+	}
+	
+	private class SpinTaskOptional<T> extends MethodBlockingTask<Optional<?>>{
+		@Override
+		protected void execute() {
+			for (int i = 0; i < 100; i++) {
+				/** intentionally empty */
+			}
+		}
+	}
 }
