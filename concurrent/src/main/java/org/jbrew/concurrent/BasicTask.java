3b4016e58d99644db3f52ffb8b88182be8c1dde3
Neal Kumar <nealk@nealk.com>
2020-03-25 01:38:53 -0400

    Release of v0.1.0-alpha.3 adds high-performance varargs implementation of enqueue() in TaskRegister.java, as well as updates Javadocs with more precise and succinct language. In addition, the maven-java-docs plugin was bumped to 3.2.0 from 3.1.1.
    Release of v0.1.0-alpha.3 adds a high-performance varargs implementation of enqueue() in TaskRegister.java
    
    Created offerTask(Task<? extends Object>...) method signature in TaskRegistry.java.
    
    Added implementation for offer(Task<?>...) in TaskRegister.java
    
    Added unit tests to TaskReisterTest.java for the enqueue(Task<?>...), as well as updated relavent Javadocs.
    
    Updated nullScan() in TaskRegister.java to use the Java 8 Streams API.
    
    Updated Javadocs for AbstractTask.java & BasicTask.java with more precise and succinct language.
    
    Bump maven-javadoc-plugin from 3.1.1 to 3.2.0.

--- concurrent/src/main/java/org/jbrew/concurrent/BasicTask.java
+++ concurrent/src/main/java/org/jbrew/concurrent/BasicTask.java
@@ -4,16 +4,19 @@ package org.jbrew.concurrent;
  * A {@link BasicTask} object is an abstract implementor of {@link org.jbrew.concurrent.Task} allowing for the
  * object to be wrapped inside of a {@link java.lang.Thread} and executed concurrently.
  * <br><br>
- * For an object blocking implementation of a {@link org.jbrew.concurrent.AbstractTask} (similar to a {@link java.util.concurrent.Callable}, please see:
+ * For an object blocking implementation of a {@link org.jbrew.concurrent.AbstractTask} (similar to a {@link java.util.concurrent.Callable}),
+ * please refer to {@link org.jbrew.concurrent.RetrievableTask}. At present it contains 2 relevant implementors:<br>
  * {@link org.jbrew.concurrent.ObjectBlockingTask} and {@link org.jbrew.concurrent.MethodBlockingTask}.
- * @author nealk
- *
+ * 
+ * @author Neal Kumar
+ * @see org.jbrew.concurrent.AbstractTask
+ * @see org.jbrew.concurrent.RetrievableTask
+ * @see org.jbrew.concurrent.ObjectBlockingTask
+ * @see org.jbrew.concurrent.MethodBlockingTask
  */
 public abstract class BasicTask extends AbstractTask<Void>{
-
 	@Override
 	public final void run() {
 		this.execute();
 	}
-	
 }
--- concurrent/src/main/java/org/jbrew/concurrent/TaskRegistry.java
+++ concurrent/src/main/java/org/jbrew/concurrent/TaskRegistry.java
@@ -12,19 +12,30 @@ package org.jbrew.concurrent;
 public interface TaskRegistry {
 	
 	/**
-	 * Inserts the specified element into this {@link TaskRegistry}. Note that the {@link TaskRegistry} is unbounded.
+	 * Inserts the specified element into this {@link TaskRegistry}. On average, the performance runtime of this
+	 * method is O(1).
 	 * 
 	 * @param task - The {@link org.jbrew.concurrent.Task} to be inserted.
+	 * @see org.jbrew.concurrent.TaskRegister#offer(Task)
 	 */
-	public abstract void offerTask(Task<?> task);
+	public abstract void offer(Task<?> task);
+	/**
+	 * Inserts the specified element into this {@link TaskRegistry}. This is the <code>varargs</code> implementation
+	 * of {@link #offer(Task)}, meaning that it handles all instances where there are either 0 or &gt;1 parameter 
+	 * arguments present.
+	 * 
+	 * @param tasks - The set of zero or more {@link org.jbrew.concurrent.Task} to be inserted into this registry.
+	 */
+	public abstract void offer(Task<?>...tasks);
 	/**
 	 * Removes a single instance of the specified {@link org.jbrew.concurrent.Task} from this {@link TaskRegistry}, 
 	 * if it is present. More formally, removes a <code>Task task</code> such that <code>task</code> is no longer 
 	 * present in the {@link TaskRegistry} if this {@link TaskRegistry} contains one or more such elements. 
 	 * 
 	 * @param task - The {@link org.jbrew.concurrent.Task} to be removed.
+	 * @return
 	 */
-	public abstract void removeTask(Task<?> task);
+	public abstract boolean remove(Task<?> task);
 	public abstract int getRegistrySize();
 	public abstract Task<?> pollTask();
 	public abstract void interruptAll();
