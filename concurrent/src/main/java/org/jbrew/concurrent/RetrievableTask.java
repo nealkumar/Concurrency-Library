84d3ee9723400f948c6486549faf82736bbe6c60
Neal Kumar <nealk@nealk.com>
2020-03-25 01:38:53 -0400

    Release of v0.1.0-alpha.3 adds high-performance varargs implementation of enqueue() in TaskRegister.java, as well as updates Javadocs with more precise and succinct language. In addition, the maven-java-docs plugin was bumped to 3.2.0 from 3.1.1.
    Release of v0.1.0-alpha.3 adds a high-performance varargs implementation of enqueue() in TaskRegister.java
    
    Created offerTask(Task<? extends Object>...) method signature in TaskRegistry.java.
    
    Added implementation for offer(Task<?>...) in TaskRegister.java
    
    Added unit tests to TaskReisterTest.java for the enqueue(Task<?>...), as well as updated relavent Javadocs.
    
    Updated nullScan() in TaskRegister.java to use the Java 8 Streams API.
    
    Updated Javadocs for AbstractTask.java & BasicTask.java with more precise and succinct language.
    
    Bump maven-javadoc-plugin from 3.1.1 to 3.2.0.

--- concurrent/src/main/java/org/jbrew/concurrent/RetrievableTask.java
+++ concurrent/src/main/java/org/jbrew/concurrent/RetrievableTask.java
@@ -1,12 +1,31 @@
 package org.jbrew.concurrent;
 
+/**
+ * A {@link org.jbrew.concurrent.AbstractTask} that returns a result. Implementors must define a single method
+ * with no arguments called <code>execute</code>. In these manners, a <code>RetrievableTask</code> is similar to a 
+ * {@link org.jbrew.concurrent.BasicTask} and {@link java.util.concurrent.Callable} in that the former is designed
+ * for classes whose instances can be potentially wrapped in another thread - while the latter also returns a result.
+ * Unlike a {@link java.util.concurrent.Callable} however, a <code>RetrievableTask</code> does not natively
+ * throw an exception, although certain flagship methods may, such as {@link RetrievableTask#retrieve()}.
+ * <br><br>
+ * For a non-returnable implementation of an {@link org.jbrew.concurrent.AbstractTask}, please refer to 
+ * {@link org.jbrew.concurrent.BasicTask}.
+ * 
+ * @author nealk
+ *
+ * @param <T> - The type of object the programmer wishes to operate upon.
+ * @see org.jbrew.concurrent.AbstractTask
+ * @see org.jbrew.concurrent.ObjectBlockingTask
+ * @see org.jbrew.concurrent.MethodBlockingTask
+ * @see org.jbrew.concurrent.BasicTask
+ */
 public abstract class RetrievableTask<T> extends AbstractTask<T>{
 	
 	protected boolean printThreadIdFlag;
 	
 	/**
 	 * <p>
-	 * 	Default constructor for RetrievableTask. This can be wrapped inside of a {@link java.lang.Thread} and executed in the same manner as a {@link java.util.concurrent.Callable}.
+	 * 	The default, noargs constructor for RetrievableTask. This can be wrapped inside of a {@link java.lang.Thread} and executed in the same manner as a {@link java.util.concurrent.Callable}.
 	 * </p>
 	 */
 	protected RetrievableTask() {
@@ -47,9 +66,10 @@ public abstract class RetrievableTask<T> extends AbstractTask<T>{
 	 * @return 	value of obj - with type &lt;T&gt;
 	 * @exception InterruptedException is thrown if the thread is interrupted. 
 	 * @author Neal Kumar
+	 * @see org.jbrew.concurrent.ObjectBlockingTask
+	 * @see org.jbrew.concurrent.MethodBlockingTask 
 	 */
 	@ThreadSafe
 	public abstract T retrieve() throws InterruptedException;
-
 
 }
--- concurrent/src/test/java/org/jbrew/concurrent_tests/register/TaskRegisterTest.java
+++ concurrent/src/test/java/org/jbrew/concurrent_tests/register/TaskRegisterTest.java
@@ -1,54 +1,86 @@
 package org.jbrew.concurrent_tests.register;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
+
 import org.jbrew.concurrent.*;
 
 @org.jbrew.Testing
 public class TaskRegisterTest {
-	
+
 	private Task<Integer> task;
-	
+
 	@org.junit.Before
 	public void setup() {
 		task = new BT<>();
 	}
-	
+
 	@Test
 	public void instantiateTaskRegisterTest() {
 		assertNotNull(new TaskRegister());
 	}
-	
+
 	@Test
-	public void removeTaskTest() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+	public void removeTaskTest() throws NoSuchFieldException, SecurityException, IllegalArgumentException,
+			IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
 		TaskRegister register = new TaskRegister();
-		register.offerTask(task);
-		//add reflection to test whether the actual PriorityBlockingQueue removes the Task<?>.
+		register.offer(task);
+		// add reflection to test whether the actual PriorityBlockingQueue removes the
+		// Task<?>.
 		Class<? extends Object> taskRegister = register.getClass();
-		Class<?>[] taskParam = {Object.class};
+		Class<?>[] taskParam = { Object.class };
 		Field fieldDefinition = taskRegister.getDeclaredField("taskQueue");
 		fieldDefinition.setAccessible(true);
 		Object fieldValue = fieldDefinition.get(taskRegister.getDeclaredConstructor(new Class<?>[] {}).newInstance());
 		Method contains = fieldValue.getClass().getDeclaredMethod("contains", taskParam);
 		assert (Boolean) contains.invoke(fieldValue, this.task) == false;
 	}
-	
+
 	@Test
 	public void offerAndCheckSizeTest() {
 		TaskRegister register = new TaskRegister();
-		register.offerTask(task);
-		register.offerTask(task);
+		register.offer(task);
+		register.offer(task);
 		assert register.getRegistrySize() == 2;
 	}
 	
+	@Test
+	public void offerAndRemoveThenCheckSizeTest() {
+		TaskRegister register = new TaskRegister();
+		Task<Integer> task2 = new BT<>();
+		register.offer(task);
+		register.offer(task2);
+		register.remove(task);
+		assert register.getRegistrySize() == 1;
+	}
+	
+	@Test
+	public void offerAndRemoveTest() {
+		TaskRegister register = new TaskRegister();
+		Task<Integer> task2 = new BT<>();
+		register.offer(task);
+		register.offer(task2);
+		assertTrue(register.remove(task));
+	}
+	
+	private Task<?> t;	//exists outside of the test method to remain unintialized / null.
+	@Test
+	public void offerNullTest() {
+		TaskRegister reg = new TaskRegister();
+		reg.offer(t);
+		assertEquals(0, reg.getRegistrySize());
+	}
+
 	/**
-	 * Ensures that {@link org.jbrew.concurrent.TaskRegister#pollTask()} correctly sorts
-	 * ands polls the highest priority Task.
+	 * Ensures that {@link org.jbrew.concurrent.TaskRegister#pollTask()} correctly
+	 * sorts ands polls the highest priority Task.
 	 */
 	@Test
 	public void offerAndPollTaskTest() {
@@ -58,19 +90,49 @@ public class TaskRegisterTest {
 		highPTask.setName("High Priority");
 		lowPTask.setPriority(1);
 		lowPTask.setName("Low Priority");
-		register.offerTask(lowPTask);
-		register.offerTask(highPTask);
+		register.offer(lowPTask);
+		register.offer(highPTask);
 		assert register.pollTask().getName() == "High Priority";
+	}
+
+	@Test
+	public void offerVarArgsTest() {
+		TaskRegister register = new TaskRegister();
+		Task<Integer> task1 = new BT<>(), task2 = new BT<>(), task3 = new BT<>();
+		register.offer(task1, task2, task3);
+		assertEquals(3, register.getRegistrySize());
 	}
 	
 	@Test
-	(expected = UnsupportedOperationException.class)
+	public void offerVarArgsEmptyTest() {
+		TaskRegister register = new TaskRegister();
+		register.offer();
+		assertEquals(0, register.getRegistrySize());
+	}
+	
+	private Task<?> t1;	//exists outside of the test method to remain unintialized / null.
+	@Test
+	public void offerVarArgsNullTest() {
+		TaskRegister register = new TaskRegister();
+		register.offer(t, t1);	//"t" declared above at offerNullTest() - line 75.
+		assertEquals(0, register.getRegistrySize());
+	}
+	
+	private Task<?>[] tArr;
+	@Test
+	public void offerVarArgsNullTest2() {
+		TaskRegister register = new TaskRegister();
+		register.offer(tArr);	//"t" declared above at offerNullTest() - line 75.
+		assertEquals(0, register.getRegistrySize());
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
 	public void interuptAllTest() {
 		TaskRegister register = new TaskRegister();
 		register.interruptAll();
 	}
-	
-	private class BT<T> extends ObjectBlockingTask<Integer>{
+
+	private class BT<T> extends ObjectBlockingTask<Integer> {
 		@Override
 		protected void execute() {
 			this.accept(69);
