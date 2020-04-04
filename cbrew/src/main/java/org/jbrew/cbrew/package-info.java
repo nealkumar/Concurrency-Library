827b369b45178b87454481c3218e72fc8cdfc584
Neal Kumar <nealk@nealk.com>
2020-04-04 12:43:10 -0400

    Created @NotPortable annotation for packages with native code, as well as added supporting documentation in package-info.java and README.md

--- cbrew/src/main/java/org/jbrew/cbrew/package-info.java
+++ cbrew/src/main/java/org/jbrew/cbrew/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * This library contains native facade implementations of the JBrew utility
+ * libraries with the Java Native Interface (JNI). This set of libraries
+ * features specific optimizations for Unix-based systems in terms of
+ * performance and memory. This is achieved through careful tuning using the C
+ * programing language to not only control for gargbage collection, but also to
+ * ensure maximum performance for elected library features. <code>CBrew</code>
+ * library
+ */
+@org.jbrew.cbrew.NotPortable
 package org.jbrew.cbrew;
