f7b64f8f0247b226fb666424aeecb764ab76e72a
Neal Kumar <nealk@nealk.com>
2020-04-05 18:02:05 -0400

    Added add() validator for testing native interface.

---
+++ native/native-examples/src/test/java/org/jbrew/jni/examples/DoMathTest.java
@@ -0,0 +1,17 @@
+package org.jbrew.jni.examples;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DoMathTest {
+	
+	@Test
+	public void addTest() {
+		System.out.println("LIbrary path = " + System.getProperty("java.library.path"));
+		int ans = new DoMath().add(20, 7);
+		System.out.println("Should be 27 from C = " + ans);
+		assertEquals(27, ans);
+	}
+
+}
