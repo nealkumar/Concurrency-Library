30cb0f3e51da7ae69c24c8335ea6bf2fb9c82565
Neal Kumar <nealk@nealk.com>
2020-04-10 01:57:26 -0400

    Added .dll support in the various required lib folders for cbrew.

--- cbrew/cbrew-validator/src/test/java/org/jbrew/cbrew/validator_tests/BasicMathTest.java
+++ cbrew/cbrew-validator/src/test/java/org/jbrew/cbrew/validator_tests/BasicMathTest.java
@@ -16,18 +16,18 @@ public class BasicMathTest {
 	}
 	
 	@Test
-	public void subTest() {
+	public void subtractTest() {
 		System.out.println("LIbrary path = " + System.getProperty("java.library.path"));
 		int ans = new BasicMathValidatorCBrew().subtractCBrew(20, 7);
-		System.out.println("Should be 27 from C = " + ans);
+		System.out.println("Should be 13 from C = " + ans);
 		assertEquals(13, ans);
 	}
 	
 	@Test
-	public void multTest() {
+	public void multiplyTest() {
 		System.out.println("LIbrary path = " + System.getProperty("java.library.path"));
 		int ans = new BasicMathValidatorCBrew().multiplyCBrew(111, 7);
-		System.out.println("Should be 27 from C = " + ans);
+		System.out.println("Should be 777 from C = " + ans);
 		assertEquals(777, ans);
 	}
 
