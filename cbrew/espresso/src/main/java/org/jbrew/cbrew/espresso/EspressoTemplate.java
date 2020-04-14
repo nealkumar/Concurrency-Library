dee7cefa994da606e2a7c1824a4725aebc5e2b15
Neal Kumar <nealk@nealk.com>
2020-04-14 19:17:05 -0400

    Created abstract EspressoTemplate

---
+++ cbrew/espresso/src/main/java/org/jbrew/cbrew/espresso/EspressoTemplate.java
@@ -0,0 +1,8 @@
+package org.jbrew.cbrew.espresso;
+
+public abstract class EspressoTemplate {
+	
+	public abstract int operation(int size, int[] arr);
+	public abstract int operation(int[] arr);
+
+}
