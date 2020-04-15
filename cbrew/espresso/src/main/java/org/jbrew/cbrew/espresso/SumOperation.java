f4295f3b8e24dc649b34142c1633a81fb9854e44
Neal Kumar <nealk@nealk.com>
2020-04-15 17:54:36 -0400

    Added agg_summer.c synchronous indexed implementation for sum op. Need to convert to pointer operation.

--- cbrew/espresso/src/main/java/org/jbrew/cbrew/espresso/SumOperation.java
+++ cbrew/espresso/src/main/java/org/jbrew/cbrew/espresso/SumOperation.java
@@ -7,29 +7,32 @@ public class SumOperation /*implements EspressoOperation*/{
 	static {
 		System.loadLibrary("agg_summer");
 	}
+	
+	private native int sumSequential(int size, int[] arr);
+	private native int sumParallel(int size, int[] arr, int numThreads);
 
 	//@Override
 	public int performOperation(int[] arr) {
 		//return new NativeAdapter().sumParallel(arr.length, arr, DEFAULT_NUM_THREADS);
-		return new NativeAdapter().sumSequential(arr.length, arr);
-		//return sumSequential(arr.length, arr);
+		//return new NativeAdapter().sumSequential(arr.length, arr);
+		return sumSequential(arr.length, arr);
 	}
 
 	//@Override
 	public int performOperation(int[] arr, boolean parallel) {
-		return new NativeAdapter().sumSequential(arr.length, arr);
-		//return sumSequential(arr.length, arr);
+		//return new NativeAdapter().sumSequential(arr.length, arr);
+		return sumSequential(arr.length, arr);
 	}
 
 	//@Override
 	public int performOperation(int numThreads, int[] arr) {
-		return new NativeAdapter().sumSequential(arr.length, arr);
-		//return sumSequential(arr.length, arr);
+		//return new NativeAdapter().sumSequential(arr.length, arr);
+		return sumSequential(arr.length, arr);
 	}
 	
-	private class NativeAdapter{
-		private native int sumSequential(int size, int[] arr);
-		private native int sumParallel(int size, int[] arr, int numThreads);
-	}
+//	private class NativeAdapter{
+//		private native int sumSequential(int size, int[] arr);
+//		private native int sumParallel(int size, int[] arr, int numThreads);
+//	}
 
 }
