6a17ba1d9d7bd3a7926a5af93f93f4d5ca155b9b
Neal Kumar <nealk@nealk.com>
2020-04-15 18:08:08 -0400

    Removed unused code from EspressoOperation.java and SumOperation.java; added updated javadocs in the aforementioned classes.

--- cbrew/espresso/src/main/java/org/jbrew/cbrew/espresso/EspressoOperation.java
+++ cbrew/espresso/src/main/java/org/jbrew/cbrew/espresso/EspressoOperation.java
@@ -3,14 +3,31 @@ package org.jbrew.cbrew.espresso;
 public interface EspressoOperation {
 	
 	/**
-	 * Performs the appropriate action on the given array, uses the default number of parallel threads.
-	 * @param arr - the integer array upon which the operation will occur.
+	 * Performs the appropriate action on the given <code>int</code>array, using the default number of parallel threads.
+	 * @param arr - the integer array upon which the operation will occur. All threads use the native 
+	 * <code>p_thread</code> library.
 	 * @return an <code>int</code> with the results of the operation.
 	 */
 	public int performOperation(int[] arr);
 	
+	/**
+	 * Performs the appropriate action on the given <code>int</code> array, with a <code>boolean</code> flag
+	 * to specify parallel versus synchronous execution.All threads use the native 
+	 * <code>p_thread</code> library.
+	 * @param arr
+	 * @param parallel
+	 * @return
+	 */
 	public abstract int performOperation(int[] arr, boolean parallel);
 	
+	/**
+	 * Performs the appropriate action on the given <code>int</code> array, with a <code>int</code> flag to
+	 * specify the number of parallel threads to spawn for the given operation. All threads use the native 
+	 * <code>p_thread</code> library.
+	 * @param numThreads
+	 * @param arr
+	 * @return
+	 */
 	public abstract int performOperation(int numThreads, int[] arr);
 	
 }
--- cbrew/espresso/src/main/java/org/jbrew/cbrew/espresso/SumOperation.java
+++ cbrew/espresso/src/main/java/org/jbrew/cbrew/espresso/SumOperation.java
@@ -1,6 +1,6 @@
 package org.jbrew.cbrew.espresso;
 
-public class SumOperation /*implements EspressoOperation*/{
+public class SumOperation implements EspressoOperation{
 	
 	private final static int DEFAULT_NUM_THREADS = 5;
 	
@@ -11,28 +11,21 @@ public class SumOperation /*implements EspressoOperation*/{
 	private native int sumSequential(int size, int[] arr);
 	private native int sumParallel(int size, int[] arr, int numThreads);
 
-	//@Override
+	@Override
 	public int performOperation(int[] arr) {
 		//return new NativeAdapter().sumParallel(arr.length, arr, DEFAULT_NUM_THREADS);
 		//return new NativeAdapter().sumSequential(arr.length, arr);
 		return sumSequential(arr.length, arr);
 	}
 
-	//@Override
+	@Override
 	public int performOperation(int[] arr, boolean parallel) {
-		//return new NativeAdapter().sumSequential(arr.length, arr);
 		return sumSequential(arr.length, arr);
 	}
 
-	//@Override
+	@Override
 	public int performOperation(int numThreads, int[] arr) {
-		//return new NativeAdapter().sumSequential(arr.length, arr);
 		return sumSequential(arr.length, arr);
 	}
-	
-//	private class NativeAdapter{
-//		private native int sumSequential(int size, int[] arr);
-//		private native int sumParallel(int size, int[] arr, int numThreads);
-//	}
 
 }
