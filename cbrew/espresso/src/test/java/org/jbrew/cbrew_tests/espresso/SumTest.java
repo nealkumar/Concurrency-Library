dd58d2bcd7f297d8f4f3423b8785d683a196e3ee
Neal Kumar <nealk@nealk.com>
2020-04-15 18:39:28 -0400

    Added unit integration tests on varrying size of the array for org.jbrew.cbrew.espresso:SumOperation.java in SumTest.java

--- cbrew/espresso/src/test/java/org/jbrew/cbrew_tests/espresso/SumTest.java
+++ cbrew/espresso/src/test/java/org/jbrew/cbrew_tests/espresso/SumTest.java
@@ -2,19 +2,149 @@ package org.jbrew.cbrew_tests.espresso;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.jbrew.cbrew.espresso.SumOperation;
 import org.jbrew.core.annotations.Testing;
+import org.junit.Ignore;
 import org.junit.Test;
 
 @Testing
 public class SumTest {
 	
 	@Test
-	public void sumTest() {
+	public void sumSeqBasicTest() {
 		int[] arr = {100, 50, 50};
 		SumOperation op = new SumOperation();
 		int sum = op.performOperation(arr);
 		assertEquals(200, sum);
+	}
+	
+	@Test
+	public void sumSeqMedTest() {
+		int[] arr = new int[1000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSum(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	@Test
+	public void sumSeqMed2Test() {
+		int[] arr = new int[10_000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSum(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	@Test
+	public void sumSeqMed3Test() {
+		int[] arr = new int[15_000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSum(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	@Test
+	public void sumLargeTest() {
+		int[] arr = new int[100_000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSumStreams(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	@Test
+	public void sumLarge2Test() {
+		int[] arr = new int[1_000_000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSumStreams(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	@Test
+	public void sumLarge3Test() {
+		int[] arr = new int[10_000_000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSumStreams(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	@Ignore /*Ignored because it takes >40 seconds to complete...*/
+	@Test
+	public void sumLarge4Test() {
+		int[] arr = new int[100_000_000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSumStreams(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	@Ignore /*Ignored because it takes >10 minutes to complete...*/
+	@Test
+	public void sumLarge5Test() {
+		int[] arr = new int[1_000_000_000];
+		for(int index=0; index<arr.length; index++) {
+			arr[index] = new Random().nextInt();
+		}
+		int trueSum = findSumStreams(arr);
+		SumOperation op = new SumOperation();
+		int testSum = op.performOperation(arr);
+		System.out.println("Expecting: " + trueSum + ", Received: " + testSum);
+		assertEquals(trueSum, testSum);
+	}
+	
+	private int findSum(int[] arr) {
+		return helper(arr, 0, 0, arr.length);
+	}
+	
+	private int findSumStreams(int[] arr) {
+		List<Integer> ints = new ArrayList<Integer>(arr.length);
+		for(int i : arr) {
+			ints.add(i);
+		}
+		return ints.parallelStream()
+				.reduce(0, Integer::sum);
+	}
+	
+	private int helper(int[] arr, int index, int sum, int bound) {
+		if(index == bound) return sum;
+		int curr = sum + arr[index];
+		return helper(arr, ++index, curr, bound);
 	}
 
 }
