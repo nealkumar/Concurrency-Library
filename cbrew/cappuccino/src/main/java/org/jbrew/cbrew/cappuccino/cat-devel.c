13e8d873bec21e06878d885ccd2bb1b2652a09e7
Neal Kumar <hacker@jbrew.org>
2020-05-10 16:57:42 -0400

    feat(cbrew.cappuccino...cat.c): Created working cat functionality in development source file. Need to now link with the JNI.

---
+++ cbrew/cappuccino/src/main/java/org/jbrew/cbrew/cappuccino/cat-devel.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define getchar() getc(stdin)
+#define putchar(x) putc((x), stdout)
+
+int
+main(int argc, char** argv){
+	FILE* fp;
+	void filecopy(FILE*, FILE*);
+	char* prog = argv[0];
+
+	if(argc == 1) filecopy(stdin, stdout);
+	else {
+		while(--argc > 0){
+			if((fp = fopen(*++argv, "r")) == NULL){
+				fprintf(stderr, "%s: can't open %s", 
+						prog, *argv);
+				exit(1);
+			} else{
+				filecopy(fp, stdout);
+				fclose(fp);
+			}
+		}
+		if(ferror(stdout)){
+			fprintf(stderr, "%s: error writing stdout\n", prog);
+			exit(2);
+		}
+	}
+}
+
+void filecopy(FILE* ifp, FILE* ofp){
+	int c;
+	while((c = getc(ifp)) != EOF) putc(c, ofp);
+}
